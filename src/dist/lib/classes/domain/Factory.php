032d5ce22c549ddcb90203b8d3207c2aa7acb7df
Konstantin Shevsky <mail@shevsky.com>
2019-07-18 14:51:57 +0500

    Фикс.

--- src/dist/lib/classes/domain/Factory.php
+++ src/dist/lib/classes/domain/Factory.php
@@ -2,16 +2,17 @@
 
 namespace Discount4Review\Domain;
 
-use Discount4Review\Persistence;
+use Discount4Review\Domain\Product\Product;
+use Discount4Review\Persistence\IFactory;
 
 class Factory implements IFactory
 {
 	/**
 	 * @param string $identify_key
-	 * @return shopDiscount4ReviewIProduct
+	 * @return Product
 	 */
 	public function createProduct($identify_key)
 	{
-		return new shopDiscount4ReviewProduct($identify_key);
+		return new Product($identify_key);
 	}
 }
