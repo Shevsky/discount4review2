3e22661b71da22607ad5d16ce3b64cf365ee99b7
Konstantin Shevsky <mail@shevsky.com>
2019-07-25 20:22:09 +0500

    Доработан автолоадер.

--- src/dist/lib/classes/Autoloader.php
+++ src/dist/lib/classes/Autoloader.php
@@ -4,7 +4,7 @@ namespace Discount4Review;
 
 class Autoloader
 {
-	private static $namespace = 'Discount4Review';
+	private static $namespace = ['Discount4Review'];
 	private static $registered;
 
 	/**
@@ -26,6 +26,43 @@ class Autoloader
 	}
 
 	/**
+	 * @param string[] $classname_parts
+	 * @return bool
+	 */
+	private static function verifyRootNamespace(&$classname_parts)
+	{
+		$root_namespace = self::$namespace;
+		if (is_string(self::$namespace))
+		{
+			$root_namespace = [$root_namespace];
+		}
+
+		foreach ($root_namespace as $root_namespace_part)
+		{
+			$namespace_part = array_shift($classname_parts);
+
+			if ($namespace_part !== $root_namespace_part)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * @param string $classname_part
+	 * @return string
+	 */
+	private static function convertClassnamePart($classname_part)
+	{
+		$classname_part = preg_replace(['/([a-z\d])([A-Z])/', '/([^_])([A-Z][a-z])/'], '$1_$2', $classname_part);
+		$classname_part = strtolower($classname_part);
+
+		return $classname_part;
+	}
+
+	/**
 	 * @param string $full_classname
 	 */
 	private static function autoload($full_classname)
@@ -36,24 +73,25 @@ class Autoloader
 			return;
 		}
 
-		$root_namespace = array_shift($classname_parts);
-		$classname = array_pop($classname_parts);
-		if ($root_namespace === self::$namespace)
+		if (!self::verifyRootNamespace($classname_parts))
 		{
-			$path_parts = [__DIR__];
-			$path_parts = array_merge($path_parts, array_map('strtolower', $classname_parts));
-			$path_parts[] = "{$classname}.php";
+			return;
+		}
 
-			$path = implode('/', $path_parts);
+		$classname = array_pop($classname_parts);
+		$path_parts = [__DIR__];
+		$path_parts = array_merge($path_parts, array_map([__CLASS__, 'convertClassnamePart'], $classname_parts));
+		$path_parts[] = "{$classname}.php";
 
-			if (file_exists($path))
-			{
-				require_once $path;
-			}
-			else
-			{
-				echo "File not found: {$path}";
-			}
+		$path = implode('/', $path_parts);
+
+		if (file_exists($path))
+		{
+			require_once $path;
+		}
+		else
+		{
+			echo "File not found: {$path}";
 		}
 	}
 }
