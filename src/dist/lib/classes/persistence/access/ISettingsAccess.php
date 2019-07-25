058b9908a834dfcb1626201b45d2c819228fd933
Konstantin Shevsky <mail@shevsky.com>
2019-07-25 20:36:25 +0500

    Добавлено представление доступа к настроек.

---
+++ src/dist/lib/classes/persistence/access/ISettingsAccess.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Shevsky\Discount4Review\Persistence\Access;
+
+interface ISettingsAccess
+{
+	/**
+	 * @param string $name
+	 * @return string[] = [
+	 *  $id => string
+	 * ]
+	 */
+	public function readSetting($name);
+
+	/**
+	 * @param string $name
+	 * @param string[] $value = [
+	 *  $id => string
+	 * ]
+	 */
+	public function writeSetting($name, $value);
+
+	/**
+	 * @param string $name
+	 * @param string[] $ids
+	 */
+	public function deleteSetting($name, array $ids = []);
+}
