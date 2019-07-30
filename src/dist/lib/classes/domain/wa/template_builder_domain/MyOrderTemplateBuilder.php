18b670a6ceed0e61b56440bc97629a59cb5ebd63
Konstantin Shevsky <mail@shevsky.com>
2019-07-30 23:12:55 +0500

    Тестовые параметры.

--- src/dist/lib/classes/domain/wa/template_builder_domain/MyOrderTemplateBuilder.php
+++ src/dist/lib/classes/domain/wa/template_builder_domain/MyOrderTemplateBuilder.php
@@ -15,6 +15,11 @@ class MyOrderTemplateBuilder extends TemplateBuilder
 		$vars = array_merge(
 			$vars,
 			[
+				'discount_value' => 5,
+				'bonus_value' => 135,
+				'count_for_discount' => 3,
+				'count_for_discount_left' => 1,
+				'count_for_discount_done' => 2,
 
 			]
 		);
