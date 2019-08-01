de0a2835d53b1bdd1d9aee4c5c8398a29b13f2ce
Konstantin Shevsky <mail@shevsky.com>
2019-08-01 23:24:41 +0500

    Доработан класс артикула.

--- src/dist/lib/classes/domain/wa/product/Sku.php
+++ src/dist/lib/classes/domain/wa/product/Sku.php
@@ -2,23 +2,41 @@
 
 namespace Shevsky\Discount4Review\Domain\Wa\Product;
 
+use Exception;
 use Shevsky\Discount4Review\Persistence\Product\ISku;
+use shopProductSkusModel;
 
 class Sku implements ISku
 {
 	private $product;
 	private $id;
 	private $sku;
+	private $product_skus_model;
 
 	/**
 	 * @param Product $product
 	 * @param mixed $data
+	 * @throws Exception
 	 */
 	public function __construct(Product $product, $data)
 	{
+		$this->product_skus_model = new shopProductSkusModel();
 		$this->product = $product;
-		$this->id = (int)$data['id'];
-		$this->sku = $data;
+
+		if (is_array($data) && array_key_exists('id', $data))
+		{
+			$this->id = (int)$data['id'];
+			$this->sku = $data;
+		}
+		elseif (is_numeric($data))
+		{
+			$this->id = (int)$data;
+			$this->sku = $this->product_skus_model->getSku($this->id);
+		}
+		else
+		{
+			throw new Exception('Неизвестные аргументы для построения экземпляра артикула');
+		}
 	}
 
 	/**
