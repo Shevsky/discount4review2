52144c5a27f069483f1f16d99e67f0ad52ae487d
Konstantin Shevsky <mail@shevsky.com>
2019-08-01 23:28:55 +0500

    Расширена конфигурация db.php.

--- src/dist/lib/config/db.php
+++ src/dist/lib/config/db.php
@@ -8,6 +8,14 @@ return array(
 			'PRIMARY' => array('name', 'id'),
 		),
 	),
+	'shop_discount4review_review' => array(
+		'id' => array('int', 11, 'null' => 0),
+		'sku_id' => array('int', 11),
+		'order_item_id' => array('int', 11),
+		':keys' => array(
+			'PRIMARY' => array('id'),
+		),
+	),
 	'shop_discount4review_storefront_settings' => array(
 		'id' => array('varchar', 255, 'null' => 0),
 		'name' => array('varchar', 50, 'null' => 0),
