78b7bda4e8080fb5a4d0898b500703870f7fcc09
Konstantin Shevsky <mail@shevsky.com>
2019-08-17 16:03:02 +0500

    Прописаны интерфейсы глобальных параметров настроек.

--- src/ts/settings/declares.ts
+++ src/ts/settings/declares.ts
@@ -9,4 +9,24 @@ interface ISettingsItem<T = any> {
 	[id: string]: TSettingsItemValue<T>;
 }
 
-interface IGlobalParams {}
+interface IStorefront {
+	id: string;
+	domain: string;
+	route: string;
+	theme: ITheme;
+}
+
+interface ITheme {
+	id: string;
+	name: string;
+	description: string;
+	url: string;
+	path: string;
+	storefronts: IStorefront[];
+}
+
+interface IGlobalParams {
+	plugin_url: string;
+	storefronts: IStorefront[];
+	themes: ITheme[];
+}
