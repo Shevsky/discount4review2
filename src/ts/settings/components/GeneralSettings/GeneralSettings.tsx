eeb80af4b9f428e153a114354e6b23b31a0ea029
Konstantin Shevsky <mail@shevsky.com>
2019-08-18 22:43:59 +0500

    Убрано лишнее.

--- src/ts/settings/components/GeneralSettings/GeneralSettings.tsx
+++ src/ts/settings/components/GeneralSettings/GeneralSettings.tsx
@@ -2,9 +2,7 @@ import React, { ReactNode } from 'react';
 import ContextComponent from 'settings/context/ContextComponent';
 import Field from 'lib/waui/Field/Field';
 import BasicCheckbox from 'lib/control/domain/BasicCheckbox/BasicCheckbox';
-import { observer } from 'mobx-react';
 
-@observer
 export default class GeneralSettings extends ContextComponent {
 	render(): ReactNode {
 		return (
