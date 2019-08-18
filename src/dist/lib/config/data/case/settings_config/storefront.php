99f96ad17b3ad576bf38d094efa8fc7f902f9b41
Konstantin Shevsky <mail@shevsky.com>
2019-08-18 21:35:53 +0500

    Добавлены новые элементы настроек к конфигурациям.

--- src/dist/lib/config/data/case/settings_config/storefront.php
+++ src/dist/lib/config/data/case/settings_config/storefront.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
+	'discount.status' => true,
+	'discount.value' => 0,
+	'discount.unit' => '%',
+	'discount.individual_status' => false,
+
 	'my_order.auto_inject_status' => true
 ];
--- src/ts/settings/model/settings/domain/StorefrontSettingsModel.ts
+++ src/ts/settings/model/settings/domain/StorefrontSettingsModel.ts
@@ -1,5 +1,10 @@
 import CommonSettingsModel from 'settings/model/settings/persistence/CommonSettingsModel';
 
 export default class StorefrontSettingsModel extends CommonSettingsModel {
+	'discount.status': boolean;
+	'discount.value': number;
+	'discount.unit': string;
+	'discount.individual_status': boolean;
+
 	'my_order.auto_inject_status': boolean;
 }
