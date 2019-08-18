8d759528227a158f48cc9a7a588ef74b0189be9c
Konstantin Shevsky <mail@shevsky.com>
2019-08-18 22:44:15 +0500

    Добавлены табы с настройками скидок/бонусов.

--- src/ts/settings/components/SpecificSettings/SpecificSettings.tsx
+++ src/ts/settings/components/SpecificSettings/SpecificSettings.tsx
@@ -1,8 +1,21 @@
 import React, { ReactNode } from 'react';
 import ContextComponent from 'settings/context/ContextComponent';
+import Tabs from 'lib/control/domain/Tabs/Tabs';
+import Tab from 'lib/waui/Tab/Tab';
+import DiscountSettings from 'settings/components/DiscountSettings/DiscountSettings';
 
 export default class SpecificSettings extends ContextComponent {
 	render(): ReactNode {
-		return <div>...</div>;
+		return (
+			<div>
+				<Tabs id="specific_settings">
+					<Tab label="Скидки">
+						<DiscountSettings />
+					</Tab>
+
+					<Tab label="Бонусы">bonuses</Tab>
+				</Tabs>
+			</div>
+		);
 	}
 }
