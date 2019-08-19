cffd33fa28ae2690cf1bde00e0c61a43866646d4
Konstantin Shevsky <mail@shevsky.com>
2019-08-19 16:38:25 +0500

    Добавлена реализация группы пользователей.

---
+++ src/dist/lib/classes/domain/wa/access/UserGroup.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Shevsky\Discount4Review\Domain\Wa\Access;
+
+use Shevsky\Discount4Review\Persistence\Access\IUserGroup;
+
+class UserGroup implements IUserGroup
+{
+	private $data;
+
+	/**
+	 * @param mixed[] $data
+	 */
+	public function __construct(array $data)
+	{
+		$this->data = $data;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getId()
+	{
+		return (int)$this->data['id'];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->data['name'];
+	}
+
+	/**
+	 * @return mixed[] = [
+	 *  'icon' => string
+	 * ]
+	 */
+	public function getParams()
+	{
+		return [
+			'system_id' => $this->data['system_id'],
+			'icon' => $this->data['icon']
+		];
+	}
+}
