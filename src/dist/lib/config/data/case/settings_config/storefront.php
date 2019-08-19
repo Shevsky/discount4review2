0520dcd6918170023d8607152d290e761101ca8c
Konstantin Shevsky <mail@shevsky.com>
2019-08-19 17:42:19 +0500

    Добавлены новые настройки к конфигурации.

--- src/dist/lib/config/data/case/settings_config/storefront.php
+++ src/dist/lib/config/data/case/settings_config/storefront.php
@@ -5,6 +5,8 @@ return [
 	'discount.value' => 0,
 	'discount.unit' => '%',
 	'discount.individual_status' => false,
+	'discount.individual_value' => [],
+	'discount.individual_unit' => [],
 
 	'my_order.auto_inject_status' => true
 ];
