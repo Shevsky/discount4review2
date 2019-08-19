483abf5486055280e3b3b2b9730523f1d9c13c7d
Konstantin Shevsky <mail@shevsky.com>
2019-08-19 17:42:32 +0500

    Добавлено редактирование индивидуальных размеров скидки.

--- src/ts/settings/components/DiscountSettings/DiscountSettings.tsx
+++ src/ts/settings/components/DiscountSettings/DiscountSettings.tsx
@@ -4,7 +4,10 @@ import Field from 'lib/waui/Field/Field';
 import StorefrontCheckbox from 'lib/control/domain/StorefrontCheckbox/StorefrontCheckbox';
 import StorefrontInputText from 'lib/control/domain/StorefrontInputText/StorefrontInputText';
 import StorefrontSelect from 'lib/control/domain/StorefrontSelect/StorefrontSelect';
+import { observer } from 'mobx-react';
+import { ISelectOption } from 'lib/waui/Select/Select';
 
+@observer
 export default class DiscountSettings extends ContextComponent {
 	render(): ReactNode {
 		return (
@@ -15,33 +18,75 @@ export default class DiscountSettings extends ContextComponent {
 
 				<Field label="Размер скидки">
 					<StorefrontInputText name="discount.value" type="float" short />{' '}
-					<StorefrontSelect
-						options={[
-							{
-								value: '%',
-								label: '%'
-							},
-							{
-								value: this.current_currency.code,
-								label: this.current_currency.sign
-							}
-						]}
-						name="discount.unit"
-						short
-					/>
+					<StorefrontSelect options={this.discount_unit_options} name="discount.unit" short />
 				</Field>
 
 				<Field
-					label="Индивидуальные значения размера скидки"
+					label="Индивидуальные значения скидки"
 					hint="Включает индивидуальные значения размера скидки, зависящие от группы пользователя"
+					appendTop
 				>
 					<StorefrontCheckbox name="discount.individual_status" />
 				</Field>
+
+				{this.is_individual_settings && (
+					<>
+						<Field
+							label=""
+							hint="Если значение не указано, будет использоваться общее значение размера скидки"
+						>
+							{this.params.user_groups.map(user_group => (
+								<Field label={user_group.name} vertical key={user_group.id}>
+									<StorefrontInputText
+										name="discount.individual_value"
+										type="float"
+										arrayAccess={{
+											index: user_group.id,
+											defaultValue: ''
+										}}
+										allowEmpty
+										short
+									/>{' '}
+									<StorefrontSelect
+										options={this.discount_unit_options}
+										name="discount.individual_unit"
+										arrayAccess={{
+											index: user_group.id,
+											defaultValue: this.default_discount_unit
+										}}
+										short
+									/>
+								</Field>
+							))}
+						</Field>
+					</>
+				)}
 			</div>
 		);
 	}
 
 	get current_currency(): ICurrency {
 		return this.params.currencies.find(currency => currency.current);
+	}
+
+	get is_individual_settings(): boolean {
+		return this.storefront_settings['discount.individual_status'];
+	}
+
+	get default_discount_unit(): string {
+		return this.storefront_settings['discount.unit'];
+	}
+
+	get discount_unit_options(): ISelectOption[] {
+		return [
+			{
+				value: '%',
+				label: '%'
+			},
+			{
+				value: this.current_currency.code,
+				label: this.current_currency.sign
+			}
+		];
 	}
 }
