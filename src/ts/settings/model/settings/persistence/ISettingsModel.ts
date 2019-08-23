f59e453b01fede9e0016e5c09f4edc1b249ddd93
Konstantin Shevsky <mail@shevsky.com>
2019-08-24 00:27:34 +0500

    К интерфейсы добавлен метод для подписки на изменения.

--- src/ts/settings/model/settings/persistence/ISettingsModel.ts
+++ src/ts/settings/model/settings/persistence/ISettingsModel.ts
@@ -1,3 +1,5 @@
+type TSettingsModelSubscribeHandler = (name: string, id: string, prevValue: any) => void;
+
 interface ISettingsModel {
 	id: string;
 
@@ -8,4 +10,6 @@ interface ISettingsModel {
 	read(name: string, id: string): any;
 
 	write(name: string, id: string, value: any): void;
+
+	subscribe(handler: TSettingsModelSubscribeHandler): void;
 }
