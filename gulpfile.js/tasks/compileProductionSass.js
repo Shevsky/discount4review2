1efc06bea511d693300b901caab5539d5051bde3
Konstantin Shevsky <mail@shevsky.com>
2019-08-26 21:40:47 +0500

    Максимальные уровни минифицирования CSS.

--- gulpfile.js/tasks/compileProductionSass.js
+++ gulpfile.js/tasks/compileProductionSass.js
@@ -4,27 +4,47 @@ const csso = require('gulp-csso');
 const cssbeautify = require('gulp-cssbeautify');
 const cssSvg = require('gulp-css-svg');
 const autoprefixer = require('gulp-autoprefixer');
-const cssmin = require('gulp-cssmin');
+const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 
 const dest_dir = `./build/css/frontend/`;
 
-const compileProductionSass = () => (
-	gulp.src(`./src/sass/*.{sass,scss}`)
+const compileProductionSass = () =>
+	gulp
+		.src(`./src/sass/*.{sass,scss}`)
 		.pipe(sass())
 		.pipe(cssSvg())
 		.pipe(autoprefixer())
 		.pipe(csso())
-		.pipe(cssbeautify(cssbeautify({
-			indent: '\t'
-		})))
+		.pipe(
+			cssbeautify(
+				cssbeautify({
+					indent: '\t'
+				})
+			)
+		)
 		.pipe(gulp.dest(dest_dir))
-		.pipe(cssmin())
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(gulp.dest(dest_dir))
-);
+		.pipe(
+			cleanCSS({
+				level: {
+					0: {
+						all: true
+					},
+					1: {
+						all: true
+					},
+					2: {
+						all: true
+					}
+				}
+			})
+		)
+		.pipe(
+			rename({
+				suffix: '.min'
+			})
+		)
+		.pipe(gulp.dest(dest_dir));
 
 compileProductionSass.displayName = `sass@compile_prod`;
 
