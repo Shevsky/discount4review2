fb255e1122da40b5375504ab15d4e4cb6ed1c198
Konstantin Shevsky <mail@shevsky.com>
2019-08-28 00:52:20 +0500

    Доделки селекта витрины/темы дизайна.

--- src/ts/settings/components/SettingsModelIdSelect/SettingsModelIdSelect.tsx
+++ src/ts/settings/components/SettingsModelIdSelect/SettingsModelIdSelect.tsx
@@ -1,7 +1,11 @@
 import React, { ReactNode } from 'react';
+import Styles from './SettingsModelIdSelect.sass';
+import ClassNames from 'classnames';
 import Select, { ISelectOption } from 'lib/waui/Select/Select';
 import ContextComponent from 'settings/context/ContextComponent';
 import Field from 'lib/waui/Field/Field';
+import { observer } from 'mobx-react';
+import Link from 'lib/waui/Link/Link';
 
 interface ISettingsModelIdSelect {
 	model: ISettingsModel;
@@ -10,13 +14,21 @@ interface ISettingsModelIdSelect {
 	[name: string]: any;
 }
 
+@observer
 export default class SettingsModelIdSelect extends ContextComponent<ISettingsModelIdSelect> {
 	render(): ReactNode {
-		const { model, fieldWrapped = true, ...props } = this.props;
+		const { model, fieldWrapped = true, className = '', ...props } = this.props;
+
+		const settingsModelIdSelectClass = ClassNames(Styles.settingsModelIdSelect, {
+			[className]: !!className
+		});
+		const linkClass = ClassNames(Styles.settingsModelIdSelect__link);
 
 		const renderedSelect = (
 			<Select
+				widthAuto
 				{...props}
+				className={settingsModelIdSelectClass}
 				options={this.options}
 				value={this.model.id}
 				onChange={this.handleChange}
@@ -27,11 +39,27 @@ export default class SettingsModelIdSelect extends ContextComponent<ISettingsMod
 			return (
 				<Field label={this.label} hint={this.hint} appendTop>
 					{renderedSelect}
+
+					{!this.is_general && this.has_modifies && (
+						<>
+							<Link onClick={this.handleClickReset} className={linkClass}>
+								Сбросить настройки до общих
+							</Link>
+						</>
+					)}
 				</Field>
 			);
 		} else {
 			return renderedSelect;
 		}
+	}
+
+	get is_general(): boolean {
+		return this.model.id === '*';
+	}
+
+	get has_modifies(): boolean {
+		return this.model.hasModifies(this.model.id);
 	}
 
 	private get model(): ISettingsModel {
@@ -47,7 +75,7 @@ export default class SettingsModelIdSelect extends ContextComponent<ISettingsMod
 	}
 
 	private get storefront_hint(): ReactNode {
-		if (this.model.id === '*') {
+		if (this.is_general) {
 			return 'Настройки ниже будут применены для всех витрин';
 		} else {
 			return (
@@ -59,7 +87,7 @@ export default class SettingsModelIdSelect extends ContextComponent<ISettingsMod
 	}
 
 	private get theme_hint(): ReactNode {
-		if (this.model.id === '*') {
+		if (this.is_general) {
 			return 'Настройки и шаблоны ниже будут применены для всех тем дизайна';
 		} else {
 			return (
@@ -71,6 +99,10 @@ export default class SettingsModelIdSelect extends ContextComponent<ISettingsMod
 	}
 
 	private get options(): ISelectOption[] {
+		return this.model === this.storefront_settings ? this.storefront_options : this.theme_options;
+	}
+
+	private get storefront_options(): ISelectOption[] {
 		return [
 			{
 				value: '*',
@@ -78,12 +110,22 @@ export default class SettingsModelIdSelect extends ContextComponent<ISettingsMod
 			},
 			...this.params.storefronts.map(storefront => ({
 				value: storefront.id,
-				label: storefront.id
+				label: `${this.model.hasModifies(storefront.id) ? '* ' : ''}${storefront.id}`
 			}))
 		];
 	}
 
+	private get theme_options(): ISelectOption[] {
+		return []; // TODO
+	}
+
 	private handleChange = (id: string) => {
 		this.model.id = id;
+	};
+
+	private handleClickReset = (): boolean => {
+		this.model.resetModifies(this.model.id);
+
+		return true;
 	};
 }
