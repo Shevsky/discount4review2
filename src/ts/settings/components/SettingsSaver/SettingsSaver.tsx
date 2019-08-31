ad65acf555187919a8d490eb26173d8278eeebd9
Konstantin Shevsky <mail@shevsky.com>
2019-09-01 03:46:58 +0500

    Доработана логика сохранения настроек.

--- src/ts/settings/components/SettingsSaver/SettingsSaver.tsx
+++ src/ts/settings/components/SettingsSaver/SettingsSaver.tsx
@@ -1,10 +1,11 @@
-import React, { ReactElement, MouseEvent } from 'react';
+import React, { ReactElement, MouseEvent, createRef, RefObject } from 'react';
 import ContextComponent from 'settings/context/ContextComponent';
 import Styles from './SettingsSaver.sass';
 import ClassNames from 'classnames';
 import BottomFixedBar from 'lib/waui/BottomFixedBar/BottomFixedBar';
 import Button from 'lib/waui/Button/Button';
 import WaRequest from 'util/WaRequest';
+import Icon from 'lib/waui/Icon/Icon';
 import { observer } from 'mobx-react';
 
 type TSettingsSaverStatus = 'general' | 'loading' | 'success' | 'error';
@@ -16,6 +17,10 @@ interface ISettingsSaverState {
 
 @observer
 export default class SettingsSaver extends ContextComponent<{}, ISettingsSaverState> {
+	private reset_message_timeout_ms = 2000;
+	private reset_message_timeout: number;
+	private status_box: RefObject<HTMLDivElement> = createRef<HTMLDivElement>();
+
 	constructor(props) {
 		super(props);
 
@@ -29,18 +34,30 @@ export default class SettingsSaver extends ContextComponent<{}, ISettingsSaverSt
 		const settingsSaverClass = ClassNames(Styles.settingsSaver);
 		const buttonBoxClass = ClassNames(Styles.settingsSaver__buttonBox);
 		const statusBoxClass = ClassNames(Styles.settingsSaver__statusBox, {
-			[Styles.settingsSaver__statusBox_visible]: this.status !== 'general'
+			[Styles.settingsSaver__statusBox_visible]: this.has_status_box,
+			[Styles.settingsSaver__statusBox_error]: this.status === 'error',
+			[Styles.settingsSaver__statusBox_success]: this.status === 'success'
 		});
+		const statusBoxIconClass = ClassNames(Styles.settingsSaver__statusBoxIcon);
 
 		return (
 			<BottomFixedBar>
 				<div className={settingsSaverClass}>
 					<div className={buttonBoxClass}>
-						<Button type={this.has_modifies ? 'yellow' : 'green'} onClick={this.handleClickSave}>
-							Сохранить
+						<Button type={this.button_type} onClick={this.handleClickSave}>
+							{' '}
+							Сохранить{' '}
 						</Button>
 					</div>
-					<div className={statusBoxClass}>{this.message}</div>
+					{this.has_status_box && (
+						<div className={statusBoxClass} ref={this.status_box}>
+							<Icon
+								className={statusBoxIconClass}
+								name={this.status === 'success' ? 'yes' : 'no'}
+							/>
+							{this.message}
+						</div>
+					)}
 				</div>
 			</BottomFixedBar>
 		);
@@ -50,12 +67,17 @@ export default class SettingsSaver extends ContextComponent<{}, ISettingsSaverSt
 		return this.settings_models.some(settings_model => settings_model.hasAnyModifies());
 	}
 
+	get has_status_box(): boolean {
+		return ['success', 'error'].includes(this.status);
+	}
+
 	get status(): TSettingsSaverStatus {
 		return this.state.status;
 	}
 
 	set status(status: TSettingsSaverStatus) {
-		this.setState({ status });
+		clearTimeout(this.reset_message_timeout);
+		this.setState({ status }, this.handleChangeStatus);
 	}
 
 	get message(): string {
@@ -64,6 +86,24 @@ export default class SettingsSaver extends ContextComponent<{}, ISettingsSaverSt
 
 	set message(message: string) {
 		this.setState({ message });
+	}
+
+	get button_type(): string {
+		if (this.status === 'loading') {
+			return 'loading';
+		} else if (this.status === 'error') {
+			return 'red';
+		}
+
+		if (this.has_modifies) {
+			return 'yellow';
+		}
+
+		return 'green';
+	}
+
+	componentDidMount() {
+		this.bindEvents();
 	}
 
 	protected get settings_models_identified(): {
@@ -77,6 +117,12 @@ export default class SettingsSaver extends ContextComponent<{}, ISettingsSaverSt
 
 	protected get settings_models(): ISettingsModel[] {
 		return Object.values(this.settings_models_identified);
+	}
+
+	protected bindEvents() {
+		this.settings_models.forEach(settings_model =>
+			settings_model.subscribe(this.handleChangeSettings)
+		);
 	}
 
 	private freezeSettingsModels() {
@@ -93,6 +139,9 @@ export default class SettingsSaver extends ContextComponent<{}, ISettingsSaverSt
 		}
 
 		this.handleBeforeSave();
+
+		this.status = 'loading';
+
 		WaRequest.post<{ test: number }>(
 			this.params.save_settings_url,
 			Object.assign(
@@ -103,10 +152,23 @@ export default class SettingsSaver extends ContextComponent<{}, ISettingsSaverSt
 			)
 		)
 			.then(data => {
-				console.log('ok', data, data.test);
+				this.settings_models.forEach(settings_model => settings_model.resetAllModifiedFlags());
+
+				this.message = 'Настройки сохранены';
+				this.status = 'success';
 			})
 			.catch(fail => {
-				console.log(fail);
+				const { message, response } = fail;
+
+				if (message) {
+					this.message = message;
+				} else {
+					this.message = 'Неизвестная ошибка';
+					// tslint:disable-next-line
+					console.warn(response);
+				}
+
+				this.status = 'error';
 			})
 			.finally(this.handleAfterSave);
 
@@ -119,5 +181,19 @@ export default class SettingsSaver extends ContextComponent<{}, ISettingsSaverSt
 
 	private handleAfterSave = () => {
 		this.unfreezeSettingsModels();
+	};
+
+	private handleChangeSettings = () => {
+		if (this.has_status_box) {
+			this.status = 'general';
+		}
+	};
+
+	private handleChangeStatus = () => {
+		if (this.has_status_box) {
+			this.reset_message_timeout = window.setTimeout(() => {
+				this.status_box.current.classList.remove(Styles.settingsSaver__statusBox_visible);
+			}, this.reset_message_timeout_ms);
+		}
 	};
 }
