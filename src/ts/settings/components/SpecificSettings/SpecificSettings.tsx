e50bd8704707ddf02a3222c2bc880d995c97bf46
Konstantin Shevsky <mail@shevsky.com>
2019-09-01 19:25:01 +0500

    Стилизация табов.

--- src/ts/settings/components/SpecificSettings/SpecificSettings.tsx
+++ src/ts/settings/components/SpecificSettings/SpecificSettings.tsx
@@ -4,17 +4,30 @@ import Tabs from 'lib/control/domain/Tabs/Tabs';
 import Tab from 'lib/waui/Tab/Tab';
 import DiscountSettings from 'settings/components/DiscountSettings/DiscountSettings';
 import BonusSettings from 'settings/components/BonusSettings/BonusSettings';
+import Icon from 'lib/waui/Icon/Icon';
 
 export default class SpecificSettings extends ContextComponent {
 	render(): ReactNode {
 		return (
 			<div>
 				<Tabs id="specific_settings">
-					<Tab label="Скидки">
+					<Tab
+						label={
+							<>
+								<Icon name="ss discounts-bw" /> Скидки
+							</>
+						}
+					>
 						<DiscountSettings />
 					</Tab>
 
-					<Tab label="Бонусы">
+					<Tab
+						label={
+							<>
+								<Icon name="ss affiliate-bw" /> Бонусы
+							</>
+						}
+					>
 						<BonusSettings />
 					</Tab>
 				</Tabs>
